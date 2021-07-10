3a7168d81c9df41c921a98dbc05b6819d26ff1b4
zym <1826264095@qq.com>
2021-07-10 22:00:58 +0800

    add hello world

--- java/com/example/Application.java
+++ java/com/example/Application.java
@@ -3,5 +3,6 @@ package com.example;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Fix Hot!");
+        System.out.println("Hello world!");
     }
 }
