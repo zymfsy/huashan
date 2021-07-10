ae0c1473e393cf40a4fd9e153aa04db68c5d081b
zym <1826264095@qq.com>
2021-07-10 22:53:39 +0800

    edit in IDEA

--- java/com/example/Application.java
+++ java/com/example/Application.java
@@ -5,5 +5,6 @@ public class Application {
         System.out.println("Fix Hot!");
         System.out.println("Hello world!");
         System.out.println("Hi !");
+        System.out.println("edit in IDEA ");
     }
 }
