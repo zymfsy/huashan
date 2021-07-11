dd85d71cda3c807cdd649eb7db3da4c478bc22b0
zym <1826264095@qq.com>
2021-07-11 22:20:15 +0800

    加载文件

--- java/com/example/Application.java
+++ java/com/example/Application.java
@@ -1,11 +1,37 @@
 package com.example;
 
+import org.springframework.util.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Fix Hot!");
-        System.out.println("Hello world!");
-        System.out.println("Hi !");
-        System.out.println("edit in IDEA ");
-        System.out.println("edit in console");
+
+    private static Map<String,List<String>> reload(String path) throws IOException {
+        File[] files = new File(path).listFiles();
+        Map<String,List<String>> map = new HashMap<>();
+        assert files != null;
+        for (File file : files) {
+            List<String> sections = Files.readAllLines(file.toPath());
+            sections = sections.stream().map(String::trim).collect(Collectors.toList());
+            sections.removeIf(StringUtils::isEmpty);
+            String title = file.getName().substring(0, file.getName().indexOf("."));
+            map.put(title,sections);
+        }
+        return map;
     }
+    public static void main(String[] args) {
+        try {
+            Map<String, List<String>> reload = reload("F:\\文档\\Markdown\\spring");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
